4f2d7fb698b20a83d2ea5bf49226042b1decc9fa
defektive <defektive@users.noreply.github.com>
2025-11-06 23:07:11 -0700

    darkmode

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -7,12 +7,19 @@ import 'bottom_nav.dart';
 class PitchCounterApp extends StatelessWidget {
   const PitchCounterApp({super.key});
 
+  bool isDarkMode(BuildContext context) {
+    return MediaQuery.of(context).platformBrightness == Brightness.dark;
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final bool systemIsDarkMode = isDarkMode(context);
+
     return MaterialApp(
       title: 'Perfect Pitch Counter',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey, brightness: (systemIsDarkMode ? Brightness.dark : Brightness.light), contrastLevel: 0.05),
       ),
       home: const BottomNav(),
     );
