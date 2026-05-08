a3ce14be9d45e4a4d2ff15d297790ed227d0e7ac
defektive <defektive@users.noreply.github.com>
2026-05-08 07:24:39 -0600

    make it work

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,12 +41,19 @@ android {
         versionName = flutter.versionName
     }
 
+    val releaseKeyAlias = keystoreProperties.getProperty("keyAlias", "")
+    val releaseKeyPassword = keystoreProperties.getProperty("keyPassword", "")
+    val releaseStoreFile = keystoreProperties.getProperty("storeFile")
+    val releaseStorePassword = keystoreProperties.getProperty("storePassword", "")
+
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+            if (releaseStoreFile?.isNotBlank() == true) {
+                keyAlias = releaseKeyAlias
+                keyPassword = releaseKeyPassword
+                storeFile = file(releaseStoreFile)
+                storePassword = releaseStorePassword
+            }
         }
     }
 
@@ -54,7 +61,11 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
         debug {
             // TODO: Add your own signing config for the release build.
